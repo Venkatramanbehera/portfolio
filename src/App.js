057abfb34c246bbf0fe769911ed7b6cc14421b88
Venkat Raman Behera <venkatramanbehera@Venkats-MacBook-Air.local>
2022-04-21 08:45:07 +0530

    about us components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Intro } from "./components";
+import { Intro, About } from "./components";
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 };
